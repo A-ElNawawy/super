59605c71f1b43f2d6323bfa601d6c67243ffd831
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-06-03 22:51:44 +0200

    masive commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Nav from './Nav/Nav'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+};
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Nav.css';
+import mastercard from './../../images/mastercard.png';
+import maestro from './../../images/maestro.png';
+import visa from './../../images/visa.png';
+import downAngle from './../../images/down-angle.png';
+import location from './../../images/location.png';
+import clockplus from './../../images/clockplus.png';
+import phone from './../../images/phone.png';
+
+import Container from './../Container/Container';
+import Button from './../Button/Button';
+
+export default function Nav() {
+  return (
+    <nav className="Nav">
+      <Container>
+        <nav className="inner-Nav">
+          <section className="currency">
+            <img className="card mastercard" src={mastercard} alt="Payment methods" />
+            <img className="card maestro" src={maestro} alt="Payment methods" />
+            <img className="card visa" src={visa} alt="Payment methods" />
+            <Button>
+              <p className="EGP">EGP</p>
+              <img className="downAngle" src={downAngle} alt="" />
+            </Button>
+          </section>
+          <section className="pages">
+            <Button><img className="location" src={location} alt="" />Stores</Button>
+            <Button><img className="clockplus" src={clockplus} alt="" />WorksTime</Button>
+            <Button><img className="phone" src={phone} alt="" />Contact us</Button>
+          </section>
+        </nav>
+      </Container>
+    </nav>
+  );
+};
